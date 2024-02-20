d779f3e8c0c695c277c11bc3c70298a76f93262a
steve <scherewaty@gmail.com>
2024-02-20 17:24:41 -0600

    challenge1

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  my-first-bash-script
+# Author:                       Steve Cherewaty
+# Date of latest revision:      02/20/2024
+# Purpose:                      Print a string to the terminal
+
+# Declaration of variables
+myVariable= "I am working perfectly!"
+
+# Main
+
+echo "Hello World"
+echo $myVariable
+
+
+
+
+
+# End
