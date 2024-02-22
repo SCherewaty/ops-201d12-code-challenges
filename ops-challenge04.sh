d2039c550cb3adf9a39e579a90daeaebf3c438ac
steve <scherewaty@gmail.com>
2024-02-22 15:30:42 -0600

    ops-challenge04.sh

---
+++ ops-challenge04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Script Name:                  Ops Challenge 3 class 4
+# Author:                       Steve Cherewaty
+# Date of latest revision:      02/22/2024
+# Purpose:                      Arrays
+# Resources:  
+
+# Declaration of variables
+
+
+
+# Declare Functions
+
+print_message() {
+    echo "var=$(last)"
+}
+
+
+# Main
+
+# echo $greeting
+# greeting="This is the login history"
+# echo $greeting
+
+# Invoking 
+print_message
+
+
+# End
+
+echo "This is the login history"
